e9730998930f43709880c3c20c10283962b46f25
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-10-08 18:46:25 +0530

    no. of cakes showed using useSelector

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const CakeContainer = () => {
+  const cakes = useSelector(state => state.cake.numOfCakes);
+  console.log(cakes)
   return (
     <div>
-      <h1>Number Of Cakes</h1>
+      <h1>Number Of Cakes-{cakes}</h1>
       <button>Buy Cakes</button>
     </div>
   )

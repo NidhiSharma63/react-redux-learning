ac78d7157cd4b11fd0e6518d5dab94407e1e73db
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-10-08 18:18:44 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 
-const BUY_CAKE= "BUY_CAKE";
+import React from 'react';
+import CakeContainer from './components/CakeContainer';
 
-// action-creator
-const buyCake = () =>{
-  return{
-    type:BUY_CAKE,
-    info:"we can add extra info here(optional)"
-  }
-}
-
-// Initial state
-
-const initialState={  
-  noOfCake:10
-}
-
-
-function App() {
-
-  const reducers = (state=initialState,action)=>{
-    switch(action.type){
-      case BUY_CAKE: return{
-        ...state, /* make a copy of STATE and update the define property */
-        noOfCake:state.noOfCake-1
-      }
-      default: return state
-    }
-  }
-
-
-  return;
+const App = () => {
+  return (
+    <div>
+      <CakeContainer/>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeContainer = () => {
+  return (
+    <div>
+      <h1>Number Of Cakes</h1>
+      <button>Buy Cakes</button>
+    </div>
+  )
+}
+
+export default CakeContainer;
